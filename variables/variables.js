a3af67a9362deecd675841311ee0c64253be817b
IamnotJamesBG <129505663+IamnotJamesBG@users.noreply.github.com>
2023-11-02 10:21:06 +0800

    Create variables.js

---
+++ variables/variables.js
@@ -0,0 +1,14 @@
+module.exports = (bot) => {
+bot.variables({
+maintenance: "true",
+antiswearsystem: "false",
+music_player: "null",
+music_ID:  "null",
+MUSIC_channel: "",
+MUSIC_embedMessage: "",
+Money: "0",
+Bank: "0",
+startingroulettenumber: "",
+roulettebet: "0",
+gamestatus: "off"
+});
